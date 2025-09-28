cf1dd76ad96a06cdb2f09ac167f1e91583e3ec17
Mark Campbell <mark.campbell@my.jcu.edu.au>
2025-09-28 17:29:26 +1000

    Add password check program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,6 @@
+password_length = 10
+password = input("Enter password: ")
+while len(password) < password_length:
+    print("Password does not satisfy requirements. Try again.")
+    password = input("Enter password: ")
+print("*" * len(password))
