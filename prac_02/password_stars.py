720fd657557e966d1bda7e1f30efe3b9416a88ca
Mark Campbell <mark.campbell@my.jcu.edu.au>
2025-09-28 18:10:45 +1000

    Update code functionality

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,10 @@
-password_length = 10
-password = input("Enter password: ")
-while len(password) < password_length:
-    print("Password does not satisfy requirements. Try again.")
+def main():
+    password_length = 10
     password = input("Enter password: ")
-print("*" * len(password))
+    while len(password) < password_length:
+        print("Password does not satisfy requirements. Try again.")
+        password = input("Enter password: ")
+    print("*" * len(password))
+
+
+main()
